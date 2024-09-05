ab918699d8b2c2ed937185f9d6b18b594d6406fc
Chanaka-Prasanna <chanakapinfo@gmail.com>
2024-09-05 13:29:52 +0530

    Solved

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import '@styles/globals.css';
 import Nav from '@components/Nav';
 import Provider from '@components/Provider';
+import { Suspense } from 'react';
 export const metadata = {
   title: 'Promptopia',
   description: 'Discover and share AI prompts',
@@ -10,15 +11,17 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <Provider>
-          <div className="main">
-            <div className="gradient" />
-          </div>
-          <main className="app">
-            <Nav />
-            {children}
-          </main>
-        </Provider>
+        <Suspense>
+          <Provider>
+            <div className="main">
+              <div className="gradient" />
+            </div>
+            <main className="app">
+              <Nav />
+              {children}
+            </main>
+          </Provider>
+        </Suspense>
       </body>
     </html>
   );
--- app/update-prompt/layout.jsx
+++
@@ -1,8 +0,0 @@
-import { Suspense } from 'react';
-import '@styles/globals.css';
-
-const RootLayout = ({ children }) => {
-  return <Suspense>{children}</Suspense>;
-};
-
-export default RootLayout;
